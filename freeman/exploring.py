938113b4028f9b46f3d32b489ef4917da400b18f
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2019-07-05 17:09:46 -0300

    Fix support for scale

--- freeman/exploring.py
+++ freeman/exploring.py
@@ -50,10 +50,10 @@ def scale_nodes(g, key, vlim=None, slim=(5, 50)):
             raise ValueError('node scale vlim must have exactly two elements')
         if (not isinstance(vlim[0], int) and not isinstance(vlim[0], float)) or (not isinstance(vlim[1], int) and not isinstance(vlim[1], float)):
             raise TypeError('both node scale vlim elements must be numeric')
+        if vlim[0] > vlim[1]:
+            raise ValueError('node scale vlim minimum must be smaller than maximum')
     if isclose(vlim[0], vlim[1]):
         raise ValueError('node scale vlim minimum and maximum are too close')
-    if vlim[0] > vlim[1]:
-        raise ValueError('node scale vlim minimum must be smaller than maximum')
 
     if not isinstance(slim, tuple):
         raise TypeError('node scale slim must be a tuple')
@@ -61,10 +61,10 @@ def scale_nodes(g, key, vlim=None, slim=(5, 50)):
         raise ValueError('node scale slim must have exactly two elements')
     if not isinstance(slim[0], int) or not isinstance(slim[1], int):
         raise TypeError('both node scale slim elements must be integers')
-    if isclose(slim[0], slim[1]):
-        raise ValueError('node scale slim minimum and maximum are too close')
     if slim[0] > slim[1]:
         raise ValueError('node scale slim minimum must be smaller than maximum')
+    if isclose(slim[0], slim[1]):
+        raise ValueError('node scale slim minimum and maximum are too close')
 
     for n, value in zip(g.nodes, vs):
         scale = (value - vlim[0]) / (vlim[1] - vlim[0])
@@ -88,10 +88,10 @@ def scale_edges(g, key, vlim=None, slim=(1, 10)):
             raise ValueError('edge scale vlim must have exactly two elements')
         if (not isinstance(vlim[0], int) and not isinstance(vlim[0], float)) or (not isinstance(vlim[1], int) and not isinstance(vlim[1], float)):
             raise TypeError('both edge scale vlim elements must be numeric')
+        if vlim[0] > vlim[1]:
+            raise ValueError('edge scale vlim minimum must be smaller than maximum')
     if isclose(vlim[0], vlim[1]):
         raise ValueError('edge scale vlim minimum and maximum are too close')
-    if vlim[0] >= vlim[1]:
-        raise ValueError('edge scale vlim minimum must be smaller than maximum')
 
     if not isinstance(slim, tuple):
         raise TypeError('edge scale slim must be a tuple')
@@ -99,10 +99,10 @@ def scale_edges(g, key, vlim=None, slim=(1, 10)):
         raise ValueError('edge scale slim must have exactly two elements')
     if not isinstance(slim[0], int) or not isinstance(slim[1], int):
         raise TypeError('both edge scale slim elements must be integers')
+    if slim[0] > slim[1]:
+        raise ValueError('edge scale slim minimum must be smaller than maximum')
     if isclose(slim[0], slim[1]):
         raise ValueError('edge scale slim minimum and maximum are too close')
-    if slim[0] >= slim[1]:
-        raise ValueError('edge scale slim minimum must be smaller than maximum')
 
     for (n, m), value in zip(g.edges, vs):
         scale = (value - vlim[0]) / (vlim[1] - vlim[0])
