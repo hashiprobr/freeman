3160a987d89f879092dffa703c1de385ae3346f2
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2019-07-05 14:28:21 -0300

    Refactor exploring extract methods

--- freeman/exploring.py
+++ freeman/exploring.py
@@ -1,24 +1,30 @@
+def extract_node(g, n, key):
+    if isinstance(key, str):
+        return g.nodes[n][key]
+    if isinstance(key, dict):
+        return key[n]
+    if callable(key):
+        return key(n)
+    return TypeError('key must be a string, a dictionary, or a callable')
+
+
+def extract_edge(g, n, m, key):
+    if isinstance(key, str):
+        return g.edges[n, m][key]
+    if isinstance(key, dict):
+        return key[n, m]
+    if callable(key):
+        return key(n, m)
+    return TypeError('key must be a string, a dictionary, or a callable')
+
+
 def extract_nodes(g, key, filter=None):
     for n in g.nodes:
         if filter is None or filter(n):
-            if isinstance(key, str):
-                yield g.nodes[n][key]
-            elif isinstance(key, dict):
-                yield key[n]
-            elif callable(key):
-                yield key(n)
-            else:
-                raise TypeError('key must be a string, a dictionary, or a callable')
+            yield extract_node(g, n, key)
 
 
 def extract_edges(g, key, filter=None):
     for n, m in g.edges:
         if filter is None or filter(n, m):
-            if isinstance(key, str):
-                yield g.edges[n, m][key]
-            elif isinstance(key, dict):
-                yield key[n, m]
-            elif callable(key):
-                yield key(n, m)
-            else:
-                raise TypeError('key must be a string, a dictionary, or a callable')
+            yield extract_edge(g, n, m, key)
